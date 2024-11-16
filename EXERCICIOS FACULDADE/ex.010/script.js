ae8cb0c0f8ad370815432f73f664dd9f838633df
Tamires Rodrigues Delmondes <89863897+tamiresrodrigue@users.noreply.github.com>
2024-11-16 19:14:15 -0300

    Delete EXERCICIOS FACULDADE directory

--- EXERCICIOS FACULDADE/ex.010/script.js
+++
@@ -1,8 +0,0 @@
-var numero1 = Number(prompt("Digite o primeiro número:"))
-var numero2 = Number(prompt("Digite o segundo número:"))
-
-if(numero1 == numero2){
-    alert("São iguais")
-}else{
-    alert("Não são iguais")
-}
--- EXERCICIOS FACULDADE/ex.012/script.js
+++
@@ -1,6 +0,0 @@
-var idade = Number(prompt("Digite sua idade"))
-if(idade >= 18){
-    alert("pode tirar habilitação")
-} else{
-    alert("não pode tirar habilitação")
-}
